16dd517e7b197ce047e7c3128a36c52a3da27cd6
Bluebeel <saikouah@gmail.com>
2018-12-20 16:17:44 +0100

    First commit

---
+++ pages/about.vue
@@ -0,0 +1,31 @@
+<template>
+  <section class="container">
+    <div>
+      <logo/>
+      <h1 class="title">
+        About
+      </h1>
+      <h2 class="subtitle">
+        Hi from {{ name }}
+      </h2>
+      <div class="links">
+        <nuxt-link to="/about" class="button--green">
+          Home Page
+        </nuxt-link>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  asyncData() {
+    return {
+      name: process.static ? 'static' : (process.server ? 'server' : 'client')
+    }
+  },
+  head: {
+    title: 'About page'
+  }
+}
+</script>
