1ac2247888491c2eeb78cee5a58bb3dbda8471f8
Bluebeel <saikouah@gmail.com>
2018-12-20 22:05:46 +0100

    Add sitemap module and update nuxt config file

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,62 +1,78 @@
-const pkg = require('./package')
-
-module.exports = {
-  mode: 'universal',
-
-  /*
-  ** Headers of the page
-  */
-  head: {
-    title: pkg.name,
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
-  },
-
-  /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#fff' },
-
-  /*
-  ** Global CSS
-  */
-  css: [
-    '~/assets/css/tailwind.css'
-  ],
-
-  /*
-  ** Plugins to load before mounting the App
-  */
-  plugins: [
-  ],
-
-  /*
-  ** Build configuration
-  */
-  build: {
+module.exports = () => {
+  const pkg = require('./package');
+  return {
+    mode: 'universal',
     /*
-    ** You can extend webpack config here
-    */
-    extend(config, ctx) {
-      // Run ESLint on save
-      if (ctx.isDev && ctx.isClient) {
-        config.module.rules.push({
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
-          exclude: /(node_modules)/
-        })
-      }
+     ** Headers of the page
+     */
+    head: {
+      title: pkg.name,
+      meta: [{
+          charset: 'utf-8'
+        },
+        {
+          name: 'viewport',
+          content: 'width=device-width, initial-scale=1'
+        },
+        {
+          hid: 'description',
+          name: 'description',
+          content: pkg.description
+        }
+      ],
+      link: [{
+        rel: 'icon',
+        type: 'image/x-icon',
+        href: '/favicon.ico'
+      }]
     },
-    filenames: {
-      app: '[name].js',
-      chunk: '[name].js'
+    /*
+     ** Customize the progress-bar color
+     */
+    loading: {
+      color: '#fff'
+    },
+    /*
+     ** Global CSS
+     */
+    css: [
+      '~/assets/css/tailwind.css'
+    ],
+
+    modules: [
+      '@nuxtjs/sitemap'
+    ],
+    /*
+     ** Plugins to load before mounting the App
+     */
+    plugins: [],
+
+    
+    sitemap: {
+      path: '/sitemap.xml',
+    },
+    /*
+     ** Build configuration
+     */
+    build: {
+      /*
+       ** You can extend webpack config here
+       */
+      extend(config, ctx) {
+        // Run ESLint on save
+        if (ctx.isDev && ctx.isClient) {
+          config.module.rules.push({
+            enforce: 'pre',
+            test: /\.(js|vue)$/,
+            loader: 'eslint-loader',
+            exclude: /(node_modules)/
+          })
+        }
+      },
+      filenames: {
+        app: '[name].js',
+        chunk: '[name].js'
+      }
     }
   }
-}
+};
--- pages/about.vue
+++ pages/about.vue
@@ -9,7 +9,7 @@
         Hi from {{ name }}
       </h2>
       <div class="links">
-        <nuxt-link to="/about" class="button--green">
+        <nuxt-link to="/" class="button--green">
           Home Page
         </nuxt-link>
       </div>
@@ -29,3 +29,41 @@ export default {
   }
 }
 </script>
+
+<style>
+/* Sample `apply` at-rules with Tailwind CSS
+.container {
+  @apply min-h-screen flex justify-center items-center text-center mx-auto;
+}
+*/
+
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
